716b582c585b667041287a43dd637aa7dbb88072
Chouhan <saloni.chouhan@capgemini.com>
2024-03-13 15:39:35 +0530

    node package manager deployemet error fixed

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
-import { Inter, Roboto_Condensed } from 'next/font/google';
+import { Inter, Lusitana, Roboto_Condensed } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const roboto = Roboto_Condensed({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
